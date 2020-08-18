07b0c0ec50886d46a7b1fa2482ba02443a6261f1
Adao Natalino <adaojosen@hotmail.com>
2020-08-18 19:46:12 +0100

    no comments

--- src/App.js
+++ src/App.js
@@ -32,16 +32,6 @@ function App() {
       <div className="app__videos">
 
         { renderVideos() }
-
-        {/* <Video
-        url="https://v16m.tiktokcdn.com/6e7efa7319d60762b0b327209bdffc96/5f3e7521/video/tos/useast2a/tos-useast2a-ve-0068c004/db5a213eeeaa467d911dff0713764c04/?a=1233&br=3422&bt=1711&cr=0&cs=0&dr=0&ds=3&er=&l=202008181305360101890370223C0D81D1&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2U0dDtzeHM3djMzaDczM0ApNGhpOjU1OTs8Nzo7Nzk7NGdoX2RwaXJkMy5fLS00MTZzczRjYC8yNWFhY2M2MDYwXmA6Yw%3D%3D&vl=&vr="
-        channel="Adao Natalino"
-        description="my description"
-        song="wow"
-        likes={123}
-        messages={10}
-        shares={3}
-        /> */}
         
       </div>
     </div>

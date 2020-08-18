d95f01a4fc26e50a9aabfc43974ffe1b036326d1
Adao Natalino <adaojosen@hotmail.com>
2020-08-18 16:32:41 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAasfMyq52IGm87Y6ghVxeWRoHXks-ruSk",
+    authDomain: "tik-tok-clone-42e12.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-42e12.firebaseio.com",
+    projectId: "tik-tok-clone-42e12",
+    storageBucket: "tik-tok-clone-42e12.appspot.com",
+    messagingSenderId: "487116065416",
+    appId: "1:487116065416:web:d47f48aa516d437964ed3d",
+    measurementId: "G-K6J5P7T7K8"
+  };
+
+  
---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Video.css"
+
+function video() {
+    
+    return (
+        <div className="video">
+            <video src="https://v16m.tiktokcdn.com/6e7efa7319d60762b0b327209bdffc96/5f3e7521/video/tos/useast2a/tos-useast2a-ve-0068c004/db5a213eeeaa467d911dff0713764c04/?a=1233&br=3422&bt=1711&cr=0&cs=0&dr=0&ds=3&er=&l=202008181305360101890370223C0D81D1&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2U0dDtzeHM3djMzaDczM0ApNGhpOjU1OTs8Nzo7Nzk7NGdoX2RwaXJkMy5fLS00MTZzczRjYC8yNWFhY2M2MDYwXmA6Yw%3D%3D&vl=&vr="></video>
+        </div>
+    )
+}
+
+export default video
